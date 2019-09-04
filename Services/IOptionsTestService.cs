bb24b61f3c7c374a2cfb056430209a1857ecf57c
Ossi Kasurinen <Ossi.Kasurinen@sievo.com>
2019-09-04 15:45:42 +0300

    Simplified the solution

--- Services/IOptionsTestService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace AzureFunctionOptionsConfigurationWithIoC.Services
-{
-    public interface IOptionsTestService
-    {
-        Task<string> DoSomethingAsync();
-    }
-}
